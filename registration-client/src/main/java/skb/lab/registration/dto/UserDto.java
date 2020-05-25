287c6a9d688a96c02739b5b242f7d085cede6f7a
MuradTcen <muradtcen@.gmail.com>
2020-05-25 06:35:42 +0500

    added validation constraints to UserDto

--- registration-client/src/main/java/skb/lab/registration/dto/UserDto.java
+++ registration-client/src/main/java/skb/lab/registration/dto/UserDto.java
@@ -2,12 +2,26 @@ package skb.lab.registration.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class UserDto {
 
+    @NotNull
+    @Min(3)
     private String login;
-    private String password;
-    private String email;
-    private String name;
 
+    @NotNull
+    @Min(8)
+    private String password;
+
+    @Email
+    @NotNull
+    private String email;
+
+    @NotNull
+    @Min(3)
+    private String name;
 }
